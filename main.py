4e095f7aaec3ed5218e1e4615a6f2eb671da0b2b
KacperKubutu <kacperkalinowski@protonmail.com>
2024-10-12 14:25:55 +0200

    the window is closing now you try to exit it by mouse

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
 
 def game_loop(screen):
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
         pygame.display.flip()
 
