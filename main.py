d8cc993e0b6e07329c768a3a5b314cf79b1ea716
KacperKubutu <kacperkalinowski@protonmail.com>
2024-10-12 14:21:14 +0200

    started writing gameloop

---
+++ main.py
@@ -0,0 +1,18 @@
+from constants import *
+import pygame
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_loop(screen)
+
+def game_loop(screen):
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
