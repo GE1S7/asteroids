ba705e5c686593d11033e9ed4b4e1e3e9552769a
KacperKubutu <kacperkalinowski@protonmail.com>
2024-10-15 15:53:26 +0200

    rotation done, working on player's speed now

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from constants import *
 import pygame
+from player import *
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock =  pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    game_loop(screen)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT / 2)
 
-def game_loop(screen):
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
